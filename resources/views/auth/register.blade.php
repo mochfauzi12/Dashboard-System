78c17cc4f9c2dbdd3bd5878f222374a2f3009dda
mochfauzi12 <mochfauzi57@gmail.com>
2023-11-25 08:51:05 +0700

    Create Register user with Boostrap tempalte

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -9,22 +9,27 @@
 		<hr>
         <div class="form-group">
 			<div class="row">
-				<div class="col"><input type="text" class="form-control" name="first_name" placeholder="First Name" required="required"></div>
-				<div class="col"><input type="text" class="form-control" name="last_name" placeholder="Last Name" required="required"></div>
+				<div class="col"><input type="text" class="form-control" name="name" placeholder="First Name" required="required"></div>
+				{{-- <div class="col"><input type="text" class="form-control" name="last_name" placeholder="Last Name" required="required"></div> --}}
 			</div>        	
         </div>
+        
         <div class="form-group">
         	<input type="email" class="form-control" name="email" placeholder="Email" required="required">
         </div>
+        
 		<div class="form-group">
             <input type="password" class="form-control" name="password" placeholder="Password" required="required">
         </div>
+        
 		<div class="form-group">
-            <input type="password" class="form-control" name="confirm_password" placeholder="Confirm Password" required="required">
-        </div>        
+            <input type="password" class="form-control" name="password_confirmation" placeholder="Confirm Password" required="required">
+        </div>   
+      
         <div class="form-group">
 			<label class="form-check-label"><input type="checkbox" required="required"> I accept the <a href="#">Terms of Use</a> &amp; <a href="#">Privacy Policy</a></label>
 		</div>
+        
 		<div class="form-group">
             <button type="submit" class="btn btn-primary btn-lg">Sign Up</button>
         </div>
