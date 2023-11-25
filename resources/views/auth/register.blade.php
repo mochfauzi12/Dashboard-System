64e649906c34731127ade8a190101bc28af66a5c
mochfauzi12 <mochfauzi57@gmail.com>
2023-11-25 14:33:14 +0700

    Logic register user & migration

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -8,6 +8,7 @@
 @endif
 <div class="signup-form">
     <form action="{{ route('register') }}" method="post">
+        @csrf
 		<h2>Sign Up</h2>
 		<p>Please fill in this form to create an account!</p>
 		<hr>
@@ -15,25 +16,25 @@
 			<div class="row">
 				<div class="col"><input type="text" class="form-control" name="name" placeholder="First Name" required="required"></div>
 				{{-- <div class="col"><input type="text" class="form-control" name="last_name" placeholder="Last Name" required="required"></div> --}}
-			</div>        	
+			</div>
         </div>
-        
+
         <div class="form-group">
         	<input type="email" class="form-control" name="email" placeholder="Email" required="required">
         </div>
-        
+
 		<div class="form-group">
             <input type="password" class="form-control" name="password" placeholder="Password" required="required">
         </div>
-        
+
 		<div class="form-group">
             <input type="password" class="form-control" name="password_confirmation" placeholder="Confirm Password" required="required">
-        </div>   
-      
+        </div>
+
         {{-- <div class="form-group">
 			<label class="form-check-label"><input type="checkbox" required="required"> I accept the <a href="#">Terms of Use</a> &amp; <a href="#">Privacy Policy</a></label>
 		</div> --}}
-        
+
 		<div class="form-group">
             <button type="submit" class="btn btn-primary btn-lg">Sign Up</button>
         </div>
